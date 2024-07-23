75668a47b6cb84c77910cf5648425b2b35871ad9
matuzalembatista <matuzalem_pereira@hotmail.com>
2024-07-22 22:04:47 -0300

    Criando as classes para imprementacao

---
+++ src/main/java/com/example/slider5/DescontoClienteVIP.java
@@ -0,0 +1,5 @@
+package com.example.slider5;
+
+public class DescontoClienteVIP {
+    
+}
---
+++ src/main/java/com/example/slider5/DescontoStragegy.java
@@ -0,0 +1,7 @@
+package com.example.slider5;
+
+public interface DescontoStragegy {
+
+    public Double calcularDesconto(double consumo, String tipoCliente);
+    
+} 
