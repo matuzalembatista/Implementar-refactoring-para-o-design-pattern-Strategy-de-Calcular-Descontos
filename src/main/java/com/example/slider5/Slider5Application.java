d4aaec9ef801434d5522708381f2f607af367ebb
matuzalembatista <matuzalem_pereira@hotmail.com>
2024-07-22 21:40:11 -0300

    Criando o projeto

---
+++ src/main/java/com/example/slider5/Slider5Application.java
@@ -0,0 +1,13 @@
+package com.example.slider5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Slider5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Slider5Application.class, args);
+	}
+
+}
